7e4845a02789a92c6f0f5aeb6dff7465f2ba82b0
Niek IJzerman <77008956+niekijzerman@users.noreply.github.com>
2024-03-01 14:25:19 +0100

    refactor: clean and restructure code
    - add doctstring
    - sort dependencies
    - restructure functions

--- src/crossing_utils.py
+++ src/crossing_utils.py
@@ -1,88 +1,172 @@
 import numpy as np
-
 import shapely.geometry as sg
 import geopandas as gpd
 import networkx as nx
-
 from scipy.spatial.distance import cdist
-from tqdm import tqdm
+from tqdm.notebook import tqdm
+tqdm.pandas()
 
 
-# Function to find closest target nodes from source nodes and subsequently create edge between tese nodes.
-# Edge is a connection between a source and a target node.
-# max_dist: maximmum distance between source and target node is max_dist.
-# max_connections: maximum number of new edges from source node.
-# include_cc_rule: Restricts nodes to be connected to the same walking network component multiple times
-def get_crossing_edges(gdf_source_nodes, gdf_target_nodes, max_dist=20, max_connections=3, crs='EPSG:28992', include_cc_rule=False):
+def get_distance_matrices(source_nodes, target_nodes):
+    """
+    Compute distance matrices between source and target nodes.
 
-    # Get distance matrices
-    dist_matrix = cdist(gdf_source_nodes[['x', 'y']].values, gdf_target_nodes[['x', 'y']].values, metric='euclidean')
+    Parameters:
+    - source_nodes (GeoDataFrame): GeoDataFrame of source nodes.
+    - target_nodes (GeoDataFrame): GeoDataFrame of target nodes.
+
+    Returns:
+    Tuple of NumPy arrays (dist_sort, dist_argsort) representing
+    sorted distances and corresponding indices.
+    """
+    source_x_y = source_nodes[['x', 'y']].values
+    target_x_y = target_nodes[['x', 'y']].values
+    dist_matrix = cdist(source_x_y, target_x_y, metric='euclidean')
     np.fill_diagonal(dist_matrix, 10**6)
     dist_sort = np.sort(dist_matrix, axis=1)
     dist_argsort = np.argsort(dist_matrix, axis=1)
+    return dist_sort, dist_argsort
 
-    # Calculate new edges
-    idxs = np.where(dist_sort < max_dist, True, False)
+
+def get_nodes_to_connect(dist_sort, dist_argsort, min_dist=0, max_dist=10):
+    """
+    Get nodes to connect based on distance matrices.
+
+    Parameters:
+    - dist_sort (numpy.ndarray): Sorted distance matrix.
+    - dist_argsort (numpy.ndarray): Indices of sorted distances.
+    - min_dist (float): Minimum distance threshold.
+    - max_dist (float): Maximum distance threshold.
+
+    Returns:
+    List of lists containing indices of nodes to connect for each source node.
+    """
+    idxs = np.where((dist_sort > min_dist) & (dist_sort < max_dist), True, False)
     nodes_to_connect = [list(row[row_idxs]) for row, row_idxs in zip(dist_argsort, idxs)]
+    return nodes_to_connect
+
+
+def create_edges_geometries(gdf_source_nodes, gdf_target_nodes, nodes_to_connect,
+                            dist_sort, max_connections):
+    """
+    Create edges and corresponding geometries.
+
+    Parameters:
+    - gdf_source_nodes (GeoDataFrame): GeoDataFrame of source nodes.
+    - gdf_target_nodes (GeoDataFrame): GeoDataFrame of target nodes.
+    - nodes_to_connect (list): List of lists containing indices of nodes to
+      connect for each source node.
+    - dist_sort (numpy.ndarray): Sorted distance matrix.
+    - max_connections (int): Maximum number of connections per source node.
+
+    Returns:
+    Tuple of lists (edges, edges_geometries) representing edge indices and
+    corresponding geometries.
+    """
+    edges, edges_geometries = [], []
+    for i in range(len(dist_sort)):
+        for j in range(len(nodes_to_connect[i])):
+            if j < max_connections:
+                edges.append([i, nodes_to_connect[i][j]])
+    for edge in edges:
+        source_node = gdf_source_nodes.iloc[edge[0]]['geometry']
+        target_node = gdf_target_nodes.iloc[edge[1]]['geometry']
+        edges_geometries.append(sg.LineString([source_node, target_node]))
+    return edges, edges_geometries
+
+
+def get_connections(gdf_source_nodes, gdf_target_nodes, max_dist=20, max_connections=3,
+                    crs='EPSG:28992', include_cc_rule=False, cc_column=None, return_gdf=True):
+    """
+    Get connections between source and target nodes.
+
+    Parameters:
+    - gdf_source_nodes (GeoDataFrame): GeoDataFrame of source nodes.
+    - gdf_target_nodes (GeoDataFrame): GeoDataFrame of target nodes.
+    - max_dist (float): Maximum distance for connections.
+    - max_connections (int): Maximum number of connections per source node.
+    - crs (str): Coordinate Reference System.
+    - include_cc_rule (bool): Whether to include connected components rule.
+    - cc_column (str): Column representing connected components.
+    - return_gdf (bool): Whether to return a GeoDataFrame.
+
+    Returns:
+    GeoDataFrame or Tuple of lists representing connections between nodes.
+    """
+    dist_sort, dist_argsort = get_distance_matrices(gdf_source_nodes, gdf_target_nodes)
+    nodes_to_connect = get_nodes_to_connect(dist_sort, dist_argsort, max_dist=max_dist)
 
     if include_cc_rule:
-        edges_geometries = []
+        edges, edges_geometries = [], []
         for source_node in tqdm(range(len(dist_sort))):
             restricted_cc, connections_count = [], 0
 
+            # Only connect multiple target nodes to source node if
+            # target nodes have different connected component.
             for target_node in nodes_to_connect[source_node]:
-                target_cc = gdf_target_nodes.iloc[target_node]['cc']
+                target_cc = gdf_target_nodes.iloc[target_node][cc_column]
                 if target_cc not in restricted_cc and connections_count < max_connections:
-                    edge = sg.LineString([gdf_source_nodes.iloc[source_node]['geometry'], gdf_target_nodes.iloc[target_node]['geometry']])
+                    edges.append([source_node, target_node])
+                    source_node_geom = gdf_source_nodes.iloc[source_node]['geometry']
+                    target_node_geom = gdf_target_nodes.iloc[target_node]['geometry']
+                    edge = sg.LineString([source_node_geom, target_node_geom])
                     edges_geometries.append(edge)
                     restricted_cc.append(target_cc)
                     connections_count += 1
-
     else:
-        edges = [[i, nodes_to_connect[i][j]] for i in range(len(dist_sort)) for j in range(len(nodes_to_connect[i])) if j < max_connections]
-        edges_geometries = [sg.LineString([gdf_source_nodes.iloc[edge[0]]['geometry'], gdf_target_nodes.iloc[edge[1]]['geometry']]) for edge in edges]
-    
-    gdf_edges = gpd.GeoDataFrame(geometry=edges_geometries, crs=crs)
-    return gdf_edges
+        edges, edges_geometries = create_edges_geometries(gdf_source_nodes, gdf_target_nodes,
+                                                          nodes_to_connect, dist_sort,
+                                                          max_connections=max_connections)
+
+    if return_gdf:
+        gdf_edges = gpd.GeoDataFrame(geometry=edges_geometries, crs=crs)
+        return gdf_edges
+    else:
+        return edges, edges_geometries
 
 
+def get_crossing_edges_from_curb_heights(gdf_source_nodes, gdf_target_nodes,
+                                         min_dist=0, max_dist=20, max_connections=3,
+                                         crs='EPSG:28992', cc_column='cc_from_sidewalk_edges'):
+    """
+    Get crossing edges based on curb heights.
 
-# Function to find closest target nodes from source nodes and subsequently create edge between tese nodes.
-# Edge is a connection between a source and a target node.
-# max_dist: maximmum distance between source and target node is max_dist.
-# max_connections: maximum number of new edges from source node.
-def get_crossing_edges_from_curb_heights(gdf_source_nodes, gdf_target_nodes, min_dist=10, max_dist=20, max_connections=3, cc_column='cc_from_sidewalk_edges', crs='EPSG:28992'):
+    Parameters:
+    - gdf_source_nodes (GeoDataFrame): GeoDataFrame of source nodes.
+    - gdf_target_nodes (GeoDataFrame): GeoDataFrame of target nodes.
+    - min_dist (float): Minimum distance for connections.
+    - max_dist (float): Maximum distance for connections.
+    - max_connections (int): Maximum number of connections per source node.
+    - crs (str): Coordinate Reference System.
+    - cc_column (str): Column representing connected components.
 
-    # Get distance matrices
-    print('Determining target nodes within distance {}m-{}m from source node...'.format(min_dist, max_dist))
-    dist_matrix = cdist(gdf_source_nodes[['x', 'y']].values, gdf_target_nodes[['x', 'y']].values, metric='euclidean')
-    np.fill_diagonal(dist_matrix, 10**6)
-    dist_sort = np.sort(dist_matrix, axis=1)
-    dist_argsort = np.argsort(dist_matrix, axis=1)
-    idxs = np.where((dist_sort > min_dist) & (dist_sort < max_dist), True, False)
-    nodes_to_connect = [list(row[row_idxs]) for row, row_idxs in zip(dist_argsort, idxs)]
-    print('Possible target nodes determined.')
+    Returns:
+    GeoDataFrame representing crossing edges.
+    """
+    dist_sort, dist_argsort = get_distance_matrices(gdf_source_nodes, gdf_target_nodes)
+    nodes_to_connect = get_nodes_to_connect(dist_sort, dist_argsort,
+                                            min_dist=min_dist, max_dist=max_dist)
 
-    # Calculate possible crossing edges
-    print('Determining possible edges from source nodes to target nodes...')
-    edges_dict = {}
+    edges_geometries, edges_dict = [], {}
     for source_node in tqdm(range(len(dist_sort))):
         source_cc = gdf_source_nodes.loc[source_node, cc_column]
         restricted_cc, connections_count = [source_cc], 0
+
         if source_cc not in edges_dict.keys():
             edges_dict[source_cc] = {}
 
-        # Loop over possible target nodes for source node
         for target_node in nodes_to_connect[source_node]:
             target_cc = gdf_target_nodes.loc[target_node, cc_column]
 
-            # Check if target node is not part of restricted connected component
+            # Only connect multiple target nodes to source node if
+            # target nodes have different connected component.
             if target_cc not in restricted_cc and connections_count < max_connections:
-                
-                pos_edge = sg.LineString([gdf_source_nodes.iloc[source_node]['centroid'], gdf_target_nodes.iloc[target_node]['centroid']])
+                source_node_geom = gdf_source_nodes.iloc[source_node]['centroid']
+                target_node_geom = gdf_target_nodes.iloc[target_node]['centroid']
+                pos_edge = sg.LineString([source_node_geom, target_node_geom])
                 pos_length = pos_edge.length
 
-                # Add crossing edge if distance between source connected component and target node is the shortest known route
+                # Only keep shortest possible edge between source node and target cc.
                 if target_node in edges_dict[source_cc].keys():
                     if edges_dict[source_cc][target_node]['length'] > pos_length:
                         edges_dict[source_cc][target_node]['edge'] = pos_edge
@@ -97,97 +181,88 @@ def get_crossing_edges_from_curb_heights(gdf_source_nodes, gdf_target_nodes, min
                     connections_count += 1
                     restricted_cc.append(target_cc)
 
-    print('Determined possible edges from source nodes to target nodes.')
+    # Create edge geometries.
+    for source_cc in edges_dict.keys():
+        for target_node in edges_dict[source_cc].keys():
+            edges_geometries.append(edges_dict[source_cc][target_node].get('edge'))
 
-    edges_geometries = [edges_dict[source_cc][target_node].get('edge') for source_cc in edges_dict.keys() for target_node in edges_dict[source_cc].keys()]
     gdf_edges = gpd.GeoDataFrame(geometry=edges_geometries, crs=crs)
     return gdf_edges
 
 
-def connect_curb_crossing_edge(gdf_source_nodes, gdf_target_nodes, walking_graph, max_dist=20, max_connections=3, crs='EPSG:28992', network_to_network=False):
+def connect_curb_crossing_edge(gdf_source_nodes, gdf_target_nodes, walking_graph, max_dist=20,
+                               max_connections=3, crs='EPSG:28992', network_to_network=False):
+    """
+    Connect curb crossing edges.
 
-    # Get distance matrices
-    dist_matrix = cdist(gdf_source_nodes[['x', 'y']].values, gdf_target_nodes[['x', 'y']].values, metric='euclidean')
-    np.fill_diagonal(dist_matrix, 10**6)
-    dist_sort = np.sort(dist_matrix, axis=1)
-    dist_argsort = np.argsort(dist_matrix, axis=1)
+    Parameters:
+    - gdf_source_nodes (GeoDataFrame): GeoDataFrame of source nodes.
+    - gdf_target_nodes (GeoDataFrame): GeoDataFrame of target nodes.
+    - walking_graph (networkx.Graph): Walking graph.
+    - max_dist (float): Maximum distance for connections.
+    - max_connections (int): Maximum number of connections per source node.
+    - crs (str): Coordinate Reference System.
+    - network_to_network (bool): Whether to connect network to network.
 
-    # Calculate new edges
-    idxs = np.where(dist_sort < max_dist, True, False)
-    nodes_to_connect = [list(row[row_idxs]) for row, row_idxs in zip(dist_argsort, idxs)]
-    edges = [[i, nodes_to_connect[i][j]] for i in range(len(dist_sort)) for j in range(len(nodes_to_connect[i])) if j < max_connections]
+    Returns:
+    GeoDataFrame representing connected edges.
+    """
+    dist_sort, dist_argsort = get_distance_matrices(gdf_source_nodes, gdf_target_nodes)
+    nodes_to_connect = get_nodes_to_connect(dist_sort, dist_argsort, max_dist=max_dist)
+    edges, _ = create_edges_geometries(gdf_source_nodes, gdf_target_nodes, nodes_to_connect,
+                                       dist_sort, max_connections=max_connections)
 
-    # Only connect the ends of a curb crossing if they are connected to different network nodes
     if len(edges) == 2:
-
-        # check length between network target nodes
         try:
-            point_1_network = gdf_target_nodes.iloc[edges[0][1]]['geometry']
-            point_2_network = gdf_target_nodes.iloc[edges[1][1]]['geometry']
-            node_1_network = (point_1_network.x, point_1_network.y)
-            node_2_network = (point_2_network.x, point_2_network.y)
+            target_node_1 = gdf_target_nodes.iloc[edges[0][1]]['geometry']
+            target_node_2 = gdf_target_nodes.iloc[edges[1][1]]['geometry']
+            node_1_network = (target_node_1.x, target_node_1.y)
+            node_2_network = (target_node_2.x, target_node_2.y)
             sp_length = nx.shortest_path_length(walking_graph, node_1_network, node_2_network)
-        except:
+        except Exception:
             sp_length = 1000
 
-        # Only connect the ends of a curb crossing if the network nodes they are connected to are at least 20 meter from eachother
+        # Only connect crossing edge if target nodes are in different connected components
+        # or the target nodes are at least 20 meters apart from eachother.
         if sp_length > 20:
+            edges_geometries = []
             if network_to_network:
-                edges_geometries = [sg.LineString([gdf_target_nodes.iloc[edges[0][1]]['geometry'], gdf_target_nodes.iloc[edges[1][1]]['geometry']])]
+                target_node_1 = gdf_target_nodes.iloc[edges[0][1]]['geometry']
+                target_node_2 = gdf_target_nodes.iloc[edges[1][1]]['geometry']
+                edges_geometries.append(sg.LineString([target_node_1, target_node_2]))
             else:
-                edges_geometries = [sg.LineString([gdf_source_nodes.iloc[edge[0]]['geometry'], gdf_target_nodes.iloc[edge[1]]['geometry']]) for edge in edges]
-                edges_geometries.append(sg.LineString([gdf_source_nodes.iloc[0]['geometry'], gdf_source_nodes.iloc[1]['geometry']]))
+                for edge in edges:
+                    source_node = gdf_source_nodes.iloc[edge[0]]['geometry']
+                    target_node = gdf_target_nodes.iloc[edge[1]]['geometry']
+                    edges_geometries.append(sg.LineString([source_node, target_node]))
+                source_node_1 = gdf_source_nodes.iloc[0]['geometry']
+                source_node_2 = gdf_source_nodes.iloc[1]['geometry']
+                edges_geometries.append(sg.LineString([source_node_1, source_node_2]))
             gdf_edges = gpd.GeoDataFrame(geometry=edges_geometries, crs=crs)
             return gdf_edges
         else:
             return gpd.GeoDataFrame()
-
-
-# # Function to find closest target nodes from source nodes and subsequently create edge between tese nodes.
-# # Edge is a connection between a source and a target node.
-# # max_dist: maximmum distance between source and target node is max_dist.
-# # max_connections: maximum number of new edges from source node.
-# # include_cc_rule: Restricts nodes to be connected to the same walking network component multiple times
-def get_connections(gdf_source_nodes, gdf_target_nodes, max_dist=20, max_connections=3, cc_column=None, include_cc_rule=False):
-
-    # Get distance matrices
-    dist_matrix = cdist(gdf_source_nodes[['x', 'y']].values, gdf_target_nodes[['x', 'y']].values, metric='euclidean')
-    np.fill_diagonal(dist_matrix, 10**6)
-    dist_sort = np.sort(dist_matrix, axis=1)
-    dist_argsort = np.argsort(dist_matrix, axis=1)
-
-    # Calculate new edges
-    idxs = np.where(dist_sort < max_dist, True, False)
-    nodes_to_connect = [list(row[row_idxs]) for row, row_idxs in zip(dist_argsort, idxs)]
-
-    if include_cc_rule:
-        edges, edges_geometries = [], []
-        for source_node in tqdm(range(len(dist_sort))):
-            restricted_cc, connections_count = [], 0
-
-            for target_node in nodes_to_connect[source_node]:
-                target_cc = gdf_target_nodes.iloc[target_node][cc_column]
-                if target_cc not in restricted_cc and connections_count < max_connections:
-                    edges.append([source_node, target_node])
-                    edge = sg.LineString([gdf_source_nodes.iloc[source_node]['geometry'], gdf_target_nodes.iloc[target_node]['geometry']])
-                    edges_geometries.append(edge)
-                    restricted_cc.append(target_cc)
-                    connections_count += 1
-
     else:
-        edges = [[i, nodes_to_connect[i][j]] for i in range(len(dist_sort)) for j in range(len(nodes_to_connect[i])) if j < max_connections]
-        edges_geometries = [sg.LineString([gdf_source_nodes.iloc[edge[0]]['geometry'], gdf_target_nodes.iloc[edge[1]]['geometry']]) for edge in edges]
-    
-    return edges, edges_geometries
+        return gpd.GeoDataFrame()
 
 
-# Function to obtain total number of intersections between line and lines in geodataframe
-def count_total_intersections_with_gdf(line, gdf, geom_column='geometry'):
+def count_line_gdf_intersections(line, gdf, geom_column='geometry'):
+    """
+    Count intersections between a line and a GeoDataFrame of lines.
+
+    Parameters:
+    - line (shapely.geometry.LineString): Line to count intersections.
+    - gdf (GeoDataFrame): GeoDataFrame of lines.
+    - geom_column (str): Column name containing geometries.
+
+    Returns:
+    Count of intersections.
+    """
     count = 0
 
     for other_line in gdf[geom_column]:
         intersections = line.intersection(other_line)
-        
+
         if intersections.is_empty:
             continue
 
